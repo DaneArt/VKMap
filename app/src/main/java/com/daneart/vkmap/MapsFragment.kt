b662f571312665fe4be9fd1fe0caa82e76761553
2003l <2002.leonid2003@gmail.com>
2020-09-18 23:27:13 +0300

    leO

--- app/src/main/java/com/daneart/vkmap/MapsFragment.kt
+++ app/src/main/java/com/daneart/vkmap/MapsFragment.kt
@@ -1,11 +1,17 @@
 package com.daneart.vkmap
-
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -13,8 +19,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var Gmap: GoogleMap
-    private val colors = arrayOf(
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private lateinit var map: GoogleMap
+    private lateinit var coordinates: LatLng
+    private var number = 0L
+    val colors = arrayOf(
         335.6F,
         202.5F,
         30F,
@@ -31,43 +40,48 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_maps, container, false)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
 
+        val root: View = inflater.inflate(R.layout.fragment_maps, container, false)
 
         return root
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        val point1 = LatLng(59.935722, 30.325729)
-        val hint = "Здесь дают макбуки"
-        val title = "HQ VK"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment = this.childFragmentManager.findFragmentById(R.id.map) as MapFragment?
+        mapFragment?.getMapAsync(this)
+        showExistMap()
+    }
 
 
-        //These coordinates represent the latitude and longitude of the Googleplex.
-        val latitude = 37.422160
-        val longitude = -122.084270
-        val zoomLevel = 15f
-        MapsInitializer.initialize(context)
-        val homeLatLng = LatLng(latitude, longitude)
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-        map.addMarker(MarkerOptions().position(homeLatLng))
-        Gmap = map
-        map.addMarker(
-            MarkerOptions().position(point1).title(title).snippet(hint)
-        )
+    private fun showExistMap() {
         createMarker(
-            googleMap = map,
-            point = point1,
-            hint = hint,
-            title = title,
-            color = colors[0]
+            map,
+            LatLng(59.968771, 30.293110),
+            "hint",
+            "category",
+            colors[0]
         )
-
 
     }
 
+
+    private fun setMapLongClick() {
+        map.setOnMapLongClickListener { latLng ->
+            map.addMarker(MarkerOptions().position(latLng))
+            coordinates = latLng
+        }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        enableMyLocation()
+        setMapLongClick()
+
+    }
 
     private fun createMarker(
         googleMap: GoogleMap,
@@ -84,17 +98,50 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         )
         googleMap.animateCamera(
             CameraUpdateFactory.newCameraPosition(
-                CameraPosition.Builder().target(point).zoom(14f).build()
+                CameraPosition.Builder().target(point).zoom(12f).build()
             )
         )
     }
 
-    private fun setMapLongClick() {
-        Gmap.setOnMapLongClickListener { latLng ->
-            Gmap.addMarker(
-                MarkerOptions()
-                    .position(latLng).title(latLng.toString())
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
             )
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+
 }
