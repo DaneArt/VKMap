41f09048b8a551ecc4c171b6dc4329aceee9f792
2003l <2002.leonid2003@gmail.com>
2020-09-18 22:39:34 +0300

    leO

--- app/src/main/java/com/daneart/vkmap/MapsFragment.kt
+++ app/src/main/java/com/daneart/vkmap/MapsFragment.kt
@@ -5,10 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -35,42 +32,39 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_maps, container, false)
-        /*val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)*/
-        val mMapView = root.findViewById<View>(R.id.map) as MapView
-        mMapView.onCreate(savedInstanceState)
-        mMapView.getMapAsync(this) //this is important
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
 
 
         return root
     }
 
-    override fun onMapReady(map: GoogleMap?) {
+    override fun onMapReady(map: GoogleMap) {
         val point1 = LatLng(59.935722, 30.325729)
         val hint = "Здесь дают макбуки"
         val title = "HQ VK"
 
-        if (map != null) {
-            //These coordinates represent the latitude and longitude of the Googleplex.
-            val latitude = 37.422160
-            val longitude = -122.084270
-            val zoomLevel = 15f
 
-            val homeLatLng = LatLng(latitude, longitude)
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-            map.addMarker(MarkerOptions().position(homeLatLng))
-            Gmap = map
-            map.addMarker(
-                MarkerOptions().position(point1).title(title).snippet(hint)
-            )
-            createMarker(
-                googleMap = map,
-                point = point1,
-                hint = hint,
-                title = title,
-                color = colors[0]
-            )
-        }
+        //These coordinates represent the latitude and longitude of the Googleplex.
+        val latitude = 37.422160
+        val longitude = -122.084270
+        val zoomLevel = 15f
+        MapsInitializer.initialize(context)
+        val homeLatLng = LatLng(latitude, longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng))
+        Gmap = map
+        map.addMarker(
+            MarkerOptions().position(point1).title(title).snippet(hint)
+        )
+        createMarker(
+            googleMap = map,
+            point = point1,
+            hint = hint,
+            title = title,
+            color = colors[0]
+        )
+
 
     }
 
